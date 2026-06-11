3fec9f6f95c4305652db9c538b69fb6eab09d4ea
Yahel Herrera <yherrerao1700@alumno.ipn.mx>
2026-06-10 19:18:00 -0600

    fix models relaciones

--- models.py
+++ models.py
@@ -3,16 +3,19 @@ from sqlalchemy.orm import relationship
 from datetime import datetime
 from database import Base
 
+
 class Joven(Base):
     __tablename__ = "jovenes"
 
     id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String)
+    nombre = Column(String, index=True)
     grupo = Column(String, default="Sin grupo")
+
     puntos_totales = Column(Integer, default=0)
     puntos_racha = Column(Integer, default=0)
     racha_actual = Column(Integer, default=0)
     racha_maxima = Column(Integer, default=0)
+
     asistencias = relationship("Asistencia", back_populates="joven")
 
 
@@ -25,14 +28,17 @@ class Evento(Base):
 
     asistencias = relationship("Asistencia", back_populates="evento")
 
+
 class Asistencia(Base):
     __tablename__ = "asistencias"
 
     id = Column(Integer, primary_key=True, index=True)
+
     joven_id = Column(Integer, ForeignKey("jovenes.id"))
     evento_id = Column(Integer, ForeignKey("eventos.id"))
+
     fecha_hora = Column(DateTime, default=datetime.now)
 
     joven = relationship("Joven", back_populates="asistencias")
     evento = relationship("Evento", back_populates="asistencias")
-
+    
